f5af162f629dc0cc580bd7d70536d38218742e38
agung-permana <agungpermana490@gmail.com>
2024-10-08 21:35:25 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
+
 use App\Http\Controllers\DivisiController;
 use Illuminate\Support\Facades\Auth;
-<<<<<<< HEAD
->>>>>>> bb58327 (crud divisi)
-=======
->>>>>>> e41322128cd3a9798ee14ece35356f93be63f7bd
 
 /*
 |--------------------------------------------------------------------------
@@ -37,18 +32,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/dashboard', function() {
     return view('layouts.main');
 });
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 Route::get('/divisi', [DivisiController::class, 'index']);
 Route::get('/divisi/tambah', [DivisiController::class, 'tambah']);
 Route::post('/divisi', [DivisiController::class, 'tambah_proses']);
->>>>>>> bb58327 (crud divisi)
-=======
-=======
->>>>>>> e41322128cd3a9798ee14ece35356f93be63f7bd
+
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/divisi', [DivisiController::class, 'index']);
     Route::get('/divisi/tambah', [DivisiController::class, 'tambah']);
@@ -56,7 +44,3 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/divisi/edit/{id}', [DivisiController::class, 'edit']);
     Route::delete('/divisi/{id}', [DivisiController::class, 'hapus']);
 });
-<<<<<<< HEAD
->>>>>>> e413221 (tambahkan alert, validasi, middleware)
-=======
->>>>>>> e41322128cd3a9798ee14ece35356f93be63f7bd
